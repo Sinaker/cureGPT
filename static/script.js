da86981855628e7a16e9f5e127174bc8ed9e5b26
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-09-13 19:31:23 +0530

    Login Integration

---
+++ static/script.js
@@ -0,0 +1,59 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+
+    dropdown.addEventListener('click', (e) => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            e.stopPropagation();
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+
+        });
+    });
+});
+let container = document.querySelector('.container')
+window.addEventListener('load', () => {
+})
+
+let submit = document.querySelector('.submit');
+var help = document.querySelector('.container-main');
+const inputPrompt = document.getElementById('prompt');
+const loadingBar = document.querySelector('.loading-bar');
+
+function startLoading() {
+    loadingBar.classList.add('loading');
+}
+
+function stopLoading() {
+    loadingBar.classList.remove('loading');
+}
+
+inputPrompt.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        Enter();
+    }
+});
+
+
+submit.addEventListener('click', Enter);
+function Enter() {
+    help.parentNode.removeChild(help);
+}
